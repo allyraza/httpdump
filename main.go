d76760d00ea1e20f732f1547499422e4bcd19e58
allyraza <saytoally@hotmail.com>
2020-09-08 19:12:57 +0400

    feat: add request dump handler

--- main.go
+++ main.go
@@ -5,20 +5,29 @@ import (
 	"net/http"
 )
 
+// RequestDumpHandler dumps request to stdout
+func RequestDumpHandler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		headers := ""
+		for k := range r.Header {
+			headers += fmt.Sprintf("%s: %s\n", k, r.Header.Get(k))
+		}
+
+		fmt.Printf("%s %s\n\n%s\n", r.Method, r.URL, headers)
+	})
+}
+
+// RequestDumpHandlerFunc dumps request to stdout
+func RequestDumpHandlerFunc(next http.HandlerFunc) http.Handler {
+	return RequestDumpHandler(http.HandlerFunc(next))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/", RequestDumpHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello"))
-
-		fmt.Printf("%s %s\n\n", r.Method, r.URL)
-		fmt.Println("HEADERS")
-		for k := range r.Header {
-			fmt.Printf("%s: %s\n", k, r.Header.Get(k))
-		}
-
-		fmt.Println("")
-	})
+	}))
 
 	http.ListenAndServe("localhost:2000", mux)
 }
